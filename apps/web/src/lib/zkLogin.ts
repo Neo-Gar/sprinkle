fe85a2c620e2a0616e752c4efd65145dc76dfb3f
Neo-Gar <nvatlecov@mail.ru>
2026-02-06 03:31:27 +0500

    Removed log

--- apps/web/src/lib/zkLogin.ts
+++ apps/web/src/lib/zkLogin.ts
@@ -130,7 +130,6 @@ export async function getZkProof({
       keyClaimName: "sub",
     }),
   });
-  console.log("response", response);
   if (!response.ok) {
     throw new Error("Failed to get zk proof");
   }
